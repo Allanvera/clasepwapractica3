e5504e29982e5bbe7926e2126807247d5c6ac25f
Allanvera <148926215+Allanvera@users.noreply.github.com>
2023-11-21 22:50:49 -0500

    Add files via upload

---
+++ clase.js
@@ -0,0 +1,9 @@
+// Archivo JavaScript para la funcionalidad del carrito
+const carrito = document.getElementById("carrito");
+
+function agregarAlCarrito(id) {
+    // Lógica para agregar productos al carrito (debes implementar esto)
+    // Por ejemplo, puedes agregar el producto al carrito y mostrarlo en el elemento "carrito"
+    // Puedes utilizar un objeto JavaScript para llevar un registro de los productos en el carrito
+    // y actualizar la vista del carrito cuando se agreguen productos.
+}
---
+++ clase.php
@@ -0,0 +1,20 @@
+<?php
+// Arreglo de productos
+$productos = array(
+    array(
+        "id" => 1,
+        "nombre" => "Producto 1",
+        "descripcion" => "Descripción del Producto 1.",
+        "precio" => 10.99,
+        "imagen" => "producto1.jpg"
+    ),
+    array(
+        "id" => 2,
+        "nombre" => "Producto 2",
+        "descripcion" => "Descripción del Producto 2.",
+        "precio" => 15.99,
+        "imagen" => "producto2.jpg"
+    ),
+    // Agrega más productos aquí
+);
+?>
